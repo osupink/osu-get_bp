266b1e78ca8de7440ab0004ad85c4b37986e84a0
lslqtz <admin@acgn.xyz>
2021-08-25 18:14:26 +0800

    Update config.php

--- config.php
+++ config.php
@@ -12,16 +12,13 @@ function get_bp($user,$mode,$limit) {
 		$cached=0;
 		$json=file_get_contents(apiurl."get_user_best?k=".osuAPIKey."&u=$user&m=$mode&limit=100",0,stream_context_create(array('http' => array('method' => 'GET','timeout' => 30))));
 	}
-	$obj=json_decode($json);
+	$obj=json_decode($json, true);
 	if (count($obj) != 0) {
 		if (!$cached && memcache && !$cacheerror) {
 			$memcache->set("$user.bp.$mode.100",$json,0,memcachetime);
 		}
-		$array=array();
-		for ($i=0;$i<min($limit,count($obj));$i++) {
-			array_push($array,array('beatmap_id' => $obj[$i]->beatmap_id,'score' => $obj[$i]->score,'maxcombo' => $obj[$i]->maxcombo,'date' => $obj[$i]->date,'pp' => $obj[$i]->pp,'rank' => $obj[$i]->rank,'count50' => $obj[$i]->count50,'count100' => $obj[$i]->count100,'count300' => $obj[$i]->count300,'countmiss' => $obj[$i]->countmiss,'countkatu' => $obj[$i]->countkatu,'countgeki' => $obj[$i]->countgeki,'perfect' => $obj[$i]->perfect));
-		}
-		return $array;
+		array_splice($obj, $limit);
+		return $obj;
 	} elseif (memcache && !$cacheerror) {
 		$memcache->set("$user.$mode.notfound",1,0,86400);
 	}
